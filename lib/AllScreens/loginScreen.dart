aab3c2744f4035d748c41fd0c5afe90db7ce326c
Matsuken108 <matsuken.96@gmail.com>
2022-11-26 22:29:18 +0900

    complete 6-48

--- lib/AllScreens/loginScreen.dart
+++ lib/AllScreens/loginScreen.dart
@@ -1,3 +1,4 @@
+import 'package:drivers_app/configMaps.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -139,11 +140,12 @@ class LoginScreen extends StatelessWidget {
         .user;
 
     if (firebaseUser != null) {
-      usersRef
+      driversRef
           .child(firebaseUser.uid)
           .once()
           .then((DataSnapshot snap) {
                 if (snap.value != null) {
+                  currentfirebaseUser = firebaseUser;
                   Navigator.pushNamedAndRemoveUntil(
                       context, MainScreen.idScreen, (route) => false);
                   displayToastMessage("you are logged-in.", context);
